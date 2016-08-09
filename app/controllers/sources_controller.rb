6bb46f2e222a2f511012216c3f4affd462a81f2f
sheebypanda <sheebypanda@gmail.com>
2016-08-09 15:15:11 +0200

    adding libs to rss parsing

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -2,13 +2,11 @@ class SourcesController < ApplicationController
   def index
     @sources = current_user.sources.all
   end
-
   def new
     @source = current_user.sources.new
   end
-
   def create
-    @source = current_user.sources.new(source_params)
+    @source = current_user.sources.new(source_url)
     if @source.save
       flash[:notice] = @source.url + " added"
       redirect_to sources_path
@@ -16,21 +14,21 @@ class SourcesController < ApplicationController
       flash[:alert] = "Error : #{@source.url} not added"
     end
   end
-
   def destroy
-    source = current_user.sources.find(params[:id])
-    if source.destroy
-      flash[:notice] = "Source successfully deleted"
+    @source = current_user.sources.find(params[:id])
+    if @source.destroy
+      flash[:notice] = "#{@source.url} successfully deleted"
     else
-      flash[:alert] = "Error : Source not deleted"
+      flash[:alert] = "Error : #{@source.url} not deleted"
     end
     redirect_to sources_path
   end
 
   private
 
-  def source_params
+  def source_url
     params.require(:source).permit(:url, :source_id)
   end
-
+  def get_rss(url)
+  end
 end
