04c6d192ee73bcdd22a105687abd27e94762bb97
sheebypanda <sheebypanda@gmail.com>
2016-08-09 10:05:47 +0200

    add source ok

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,18 +1,26 @@
 class SourcesController < ApplicationController
   def index
-    @sources = current_user.sources
+    @sources = current_user.sources.all
   end
 
   def new
-    @source = Source.new
+    @source = current_user.sources.new
   end
 
   def create
-    current_user.source.create(source_url).save
-
+    @source = current_user.sources.new(source_url)
+    if @source.save
+      flash[:notice] = @source.url + " added"
+      redirect_to sources_path
+    else
+      flash[:alert] = "Error : #{@source.url} not added"
+    end
   end
 
   def destroy
+    @source.destroy
+    flash[:notice] = "Source successfully deleted"
+    redirect_to sources_path
   end
 
   private
